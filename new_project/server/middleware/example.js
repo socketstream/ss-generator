fe13f59cded1ba1492b2f183789ff433170750f4
Paul Jensen <paulbjensen@gmail.com>
2015-04-10 17:58:11 +0100

    Made linting fixes

--- new_project/server/middleware/example.js
+++ new_project/server/middleware/example.js
@@ -4,7 +4,7 @@
 // Only let a request through if the session has been authenticated
 exports.authenticated = function() {
   return function(req, res, next) {
-    if (req.session && (req.session.userId != null)) {
+    if (req.session && req.session.userId) {
       return next();
     } else {
       return res(false);
